22dbc6e41b67982968ba081ae12e806063491637
Lucy Dykhouse <l.dykhouse24@gmail.com>
2021-10-22 11:48:56 -0400

    Finished home.js

--- home.js
+++ home.js
@@ -21,10 +21,10 @@
     'Welcome back, Andrew'
 */
 
-//CODE HERE
+const greetUser = (username) => console.log(`Welcome back, ${username}`);
 
-
-
+// Print statement
+// console.log(greetUser('Andrew'));
 
 
 //////////////////PROBLEM 2////////////////////
@@ -49,8 +49,17 @@
 
 const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 
-//CODE HERE
+const canWeDeliver = (zipcode) => {
+    if(deliveryAreaZipCodes.includes(zipcode)) {
+        return `You're in our delivery zone!`;
+    } else {
+        return `Sorry, we can't deliver to that address`;
+    }
+}
 
+//Print statements
+// console.log(canWeDeliver(84606));
+// console.log(canWeDeliver(85205));
 
 
 /* 
@@ -70,7 +79,18 @@ const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
     Name your new function `canWeDeliverTwo`.
 */
 
-// CODE HERE
+const canWeDeliverTwo = function (zipcode) {
+    for (let i = 0; i < deliveryAreaZipCodes.length; i++) {
+        if(zipcode === deliveryAreaZipCodes[i]) {
+            return `You're in our delivery zone!`;
+        }
+    }
+    return `Sorry, we can't deliver to that address`;
+}
+
+//Print statements
+// console.log(canWeDeliverTwo(84606));
+// console.log(canWeDeliverTwo(85205));
 
 
 //////////////////PROBLEM 3////////////////////
@@ -106,8 +126,10 @@ const deals = [
     to replace the 15 with a 10.
 */
 
-//CODE HERE
+deals[0].title = deals[0].title.replace('15','10');
 
+// Print statement
+// console.log(deals);
 
 
 /*
@@ -123,4 +145,7 @@ const deals = [
     to be displaying wrong on the live site.
 */
 
-//CODE HERE
+deals[1].desc = deals[1].desc.replace('March', 'April').trim();
+
+// Print statement
+// console.log(deals);
